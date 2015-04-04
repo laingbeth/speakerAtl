9f1de0708559cfec45689349b9d47ddee1d2825c
beth laing <laingbeth@gmail.com>
2015-04-04 12:19:27 -0400

    login working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
-  has_secure_password
 
-  attr_accessor :email, :password, :password_confirmation
+   before_save { self.email = email.downcase }
+
+  #attr_accessor :email, :password, :password_confirmation
 
   validates_uniqueness_of :email
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, presence: true, length: { maximum: 255 },
+  #                   format: { with: VALID_EMAIL_REGEX },
+  #                   uniqueness: true
+  has_secure_password
+
 end
